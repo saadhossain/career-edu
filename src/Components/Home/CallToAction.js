49860b80cfd0123b3145cb79ed9d590c9e85030c
Saad Hossain <mshsarkar@gmail.com>
2023-02-05 21:56:21 +0600

    Animation added on the site

--- src/Components/Home/CallToAction.js
+++ src/Components/Home/CallToAction.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import Flip from 'react-reveal/Flip';
 import { Link } from 'react-router-dom';
 import callToAction from '../../assests/career-edu-calltoaction.jpg';
+import Slide from 'react-reveal/Slide'
 
 const CallToAction = () => {
     return (
         <div className='w-11/12 lg:w-10/12 mx-auto my-5 lg:my-16 lg:flex items-center justify-between'>
             <div className='w-full lg:w-2/4'>
-                <img src={callToAction} alt='Call To Action' className='rounded-xl' />
+                <Flip left>
+                    <img src={callToAction} alt='Call To Action' className='rounded-xl' />
+                </Flip>
             </div>
             <div className='w-full lg:w-2/4 lg:text-end'>
-                <h2 className='text-3xl mt-3 lg:mt-0 lg:text-5xl font-bold text-edu'>Are You Ready To Transform Your Life?</h2>
-                <Link to='/courses'>
-                    <button className='bg-edu py-2 px-5 rounded mt-5 text-white font-semibold duration-300 ease-in-out hover:bg-eduhover'>Get Enrolled</button>
-                </Link>
+                <Slide right>
+                    <h2 className='text-3xl mt-3 lg:mt-0 lg:text-5xl font-bold text-edu'>Are You Ready To Transform Your Life?</h2>
+                </Slide>
+                <Slide bottom>
+                    <Link to='/courses'>
+                        <button className='bg-edu py-2 px-5 rounded mt-5 text-white font-semibold duration-300 ease-in-out hover:bg-eduhover'>Get Enrolled</button>
+                    </Link>
+                </Slide>
             </div>
         </div>
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+import { Zoom } from 'react-reveal';
+import Fade from 'react-reveal/Fade';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
@@ -21,41 +23,45 @@ const Home = () => {
         autoplaySpeed: 3000,
         responsive: [
             {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                initialSlide: 2
-              }
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
             },
             {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-          ]
+        ]
     };
     return (
         <div>
             {/* Home Banner */}
-            <img src={homeBanner} alt='Home Banner' className='max-h-screen w-full' />
+            <Fade left>
+                <img src={homeBanner} alt='Home Banner' className='max-h-screen w-full' />
+            </Fade>
             {/* Featured Courses */}
             <div className='bg-[#d8e4ff] py-5'>
                 <h2 className='text-edu text-2xl md:text-4xl font-bold text-center mb-5'>Featured Courses</h2>
                 <div className='w-11/12 md:w-10/12 mx-auto'>
-                    <Slider {...settings}>
-                        {
-                            featuredCourse.map(course => <CoursesShowCase key={course.id} course={course}></CoursesShowCase>)
-                        }
-                    </Slider>
+                    <Zoom>
+                        <Slider {...settings}>
+                            {
+                                featuredCourse.map(course => <CoursesShowCase key={course.id} course={course}></CoursesShowCase>)
+                            }
+                        </Slider>
+                    </Zoom>
                 </div>
             </div>
             {/* Call To Action Section */}
             <CallToAction />
             {/* Features Section */}
-            <Features/>
+            <Features />
         </div>
     );
 };
